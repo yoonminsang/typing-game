88de43aaa4f5562969ad37b3b0a91e4fa741ef0d
yoonminsang <apdlyooapdl@naver.com>
2021-12-15 14:14:37 +0900

    setting: 웹팩 설정

---
+++ config/webpack.common.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, '../dist'),
+    clean: true,
+    assetModuleFilename: 'assets/[hash][ext][query]',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    modules: ['node_modules'],
+    plugins: [new TsconfigPathsPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-typescript',
+              [
+                '@babel/preset-env',
+                {
+                  targets: '> 1%, not dead',
+                  useBuiltIns: 'usage',
+                  corejs: { version: '3' },
+                },
+              ],
+            ],
+          },
+        },
+        exclude: /(node_modules)/,
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      // favicon: './src/favicon.png',
+    }),
+    new CompressionPlugin(),
+  ],
+};
