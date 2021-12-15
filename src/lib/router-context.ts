e023b1d744bb39dda6cd6b5e1ee8e122184c2d85
yoonminsang <apdlyooapdl@naver.com>
2021-12-16 04:58:41 +0900

    wip: 테스트때문에 router context push 수정

--- src/lib/router-context.ts
+++ src/lib/router-context.ts
@@ -5,7 +5,15 @@ class RouterContext {
   state: IRouterState;
 
   constructor() {
-    this.state = { pathname: getPathname(), query: getQuery(), params: {}, push: () => {}, goBack: () => {} };
+    this.state = {
+      pathname: getPathname(),
+      query: getQuery(),
+      params: {},
+      push: (url) => {
+        window.history.pushState(null, '', url);
+      },
+      goBack: () => {},
+    };
   }
 
   public setState(nextState: object) {
