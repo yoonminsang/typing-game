548885d772066524136fd5c41f2c59958d22b740
yoonminsang <apdlyooapdl@naver.com>
2021-12-15 14:31:54 +0900

    feat: 엔트리 포인트 생성

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import Router from './lib/router';
+import NotFoundPage from './pages/not-found-page';
+import { IRoute } from './types/route';
+import { getWords } from './utils/api/words';
+import { addLoader } from './utils/loader';
+
+class App {
+  target: HTMLElement;
+  routes: IRoute[];
+  NotFoundPage: any;
+
+  constructor(target: HTMLElement) {
+    this.target = target;
+    this.routes = [];
+    this.NotFoundPage = NotFoundPage;
+    this.init();
+    this.render();
+  }
+
+  init() {
+    addLoader();
+  }
+
+  render() {
+    new Router(this.target, this.routes, this.NotFoundPage);
+  }
+}
+
+export default App;
---
+++ src/types/route.ts
@@ -0,0 +1,5 @@
+// TODO: 컴포넌트 타입지정
+export interface IRoute {
+  path: string;
+  component: any;
+}
