7699a8ab247a1457368bd49013e4735d90b83f06
yoonminsang <apdlyooapdl@naver.com>
2021-12-15 18:50:51 +0900

    chore: 함수명 변경

--- src/utils/api/words.ts
+++ src/utils/api/words.ts
@@ -2,4 +2,4 @@ import { AxiosResponse } from 'axios';
 import { IWord } from '@/types/words';
 import request from './request';
 
-export const getWords = (): Promise<AxiosResponse> => request<IWord[]>('GET', `/kakaopay-fe/resources/words`);
+export const getWordsApi = (): Promise<AxiosResponse> => request<IWord[]>('GET', `/kakaopay-fe/resources/words`);
